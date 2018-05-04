cd7ec65c74a59756499ac80f365a4f76520cfe11
Max Harris <maxreuven@gmail.com>
2018-05-04 04:47:57 -0700

    lessed up

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,47 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-  	uglify: {
-		    build: {
-		      src: 'js/src/*.js',
-		      dest: 'js/min/scripts.min.js'
-		    }
+		less: {
+			dev: {
+				options: {
+					plugins: [
+						new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]}),
+          	require('less-plugin-group-css-media-queries')
+					]
+				},
+				files: {
+					'style.css': 'assets/less/style.less'
+				}
 			}
+		},
+  	uglify: {
+	    build: {
+	      src: 'assets/js/src/*.js',
+	      dest: 'production-assets/js/scripts.min.js'
+	    }
+		},
+
+		watch: {
+			css: {
+				files: ['assets/less/*.less'],
+				tasks: ['less'],
+				options: {
+					livereload: true
+				}
+			},
+			scripts: {
+				files: ['assets/js/*.js'],
+				tasks: ['uglify'],
+				options: {
+					livereload: true
+				}
+			}
+		}
   });
 
-	grunt.registerTask('default', ['uglify']);
-
+	grunt.loadNpmTasks('grunt-contrib-less-compiler');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
+	grunt.registerTask('default', ['less', 'uglify']);
 };
